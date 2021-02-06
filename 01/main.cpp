c1346f0316a410787759819e5c2ce33a98f399fc
Den <denchikchiter@gmail.com>
2021-02-06 20:31:46 +0200

    Works

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,7 +1,46 @@
+#include "matrix.h"
+#include <fstream>
 #include <iostream>
+#include <random>
 
-auto main() -> int 
+//using namespace std;
+
+void fill_matrixs(Matrix &X, Matrix& Y)
 {
-    std::cout << "Hello";
-    std::cout << ",  world!\n";
+  std::ifstream data("ACR.txt");
+  for (int i = 0; i < X.GetNumRows(); i++)
+  {
+    for (int j = 0; j < X.GetNumColumns(); j++)
+      data >> X[i][j];
+    data >> Y[i][0];
+  }
+}
+
+int main()
+{
+  Matrix X(100, 7);
+  Matrix Y(100, 1);
+  fill_matrixs(X, Y);
+  Matrix XT = Transp(X);
+  Matrix XTX = XT*X;
+  Matrix result = MatrixE(XTX) * XT * Y;
+  std::cout << result;
+
+}
+
+void func()
+{
+  int p = 3, q = 2;
+  std::default_random_engine generator;
+  std::normal_distribution<double> distribution(5.0, 2.0);
+  double mass[10] = {};
+  for (int i = 0; i < 1000; ++i)
+  {
+    double number = distribution(generator);
+    if ((number >= 0.0) && (number < 10.0))
+      ++mass[int(number)];
+  }
+  vector<double> a({mass[0], mass[1], mass[2]});
+  for (auto &v : mass)
+    std::cout << v << ' ';
 }
---
+++ 01/matrix.h
@@ -0,0 +1,45 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+
+using std::vector;
+
+class Matrix {
+private:
+  int num_rows;
+  int num_columns;
+
+  vector<vector<double>> elements;
+
+public:
+  Matrix();
+  Matrix(vector<vector<double>>&& elements_);
+  Matrix(int num_rows, int num_columns);
+  void Reset(int num_rows_, int num_columns_);
+  int GetNumRows() const;
+  int GetNumColumns() const;
+  const std::vector<double>& operator[](size_t i) const;
+  vector<double>& operator[](size_t i);
+};
+
+bool operator==(const Matrix& one, const Matrix& two);
+Matrix operator+(const Matrix& one, const Matrix& two);
+Matrix operator*(const Matrix& one, const Matrix& two);
+Matrix Transp(const Matrix& matrix);
+Matrix MatrixE(const Matrix&);
+
+double operator*(const vector<double>& one, const vector<double>& two);
+const vector<double> operator-(const vector<double>& one, const vector<double>& two);
+const vector<double> operator*(const double one, const vector<double>& two);
+
+std::istream& operator>>(std::istream& in, Matrix& matrix);
+std::ostream& operator<<(std::ostream& out, const Matrix& matrix);
+
+
+#endif
