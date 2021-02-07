04ac74d0d38a1173929fa640cf003d7dcdd9061e
Den <denchikchiter@gmail.com>
2021-02-07 20:12:47 +0200

    Everything works

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,7 +1,9 @@
 #include "matrix.h"
+#include <cmath>
 #include <ctime>
 #include <fstream>
 #include <iostream>
+
 //#include <random>
 
 // using namespace std;
@@ -9,18 +11,26 @@
 void create_data(const int N);
 void fill_matrixs(Matrix &X, Matrix &Y);
 Matrix count_omega(Matrix &X, Matrix &Y, int params_count, int betta);
+std::pair<double, double> Akaiki(Matrix &, Matrix &, Matrix &);
 
 int main() {
   srand(time(NULL));
   const int N = 1000;
   const int params_count = 8;
-  // create_data(N);
+  create_data(N);
   Matrix X(N, params_count);
   Matrix Y(N, 1);
   fill_matrixs(X, Y);
-  Matrix result = MatrixE(Transp(X) * X) * Transp(X) * Y;
-  Matrix O = count_omega(X, Y, params_count, 1000);
-  std::cout << "Omega:\n" << O << "Etalon:\n" << result;
+  Matrix MHK = MatrixE(Transp(X) * X) * Transp(X) * Y;
+  Matrix PMHK = count_omega(X, Y, params_count, 10'000'000);
+  std::cout << "MHK algorithm:\n" << MHK << "PMHK algorithm:\n" << PMHK;
+
+  auto Akaiki_MHK = Akaiki(X, Y, MHK);
+  std::cout << "Sum E^2 for MHK: " << Akaiki_MHK.first << '\n'
+            << "Akaiki for MHK: " << Akaiki_MHK.second << '\n';
+  auto Akaiki_PMHK = Akaiki(X, Y, PMHK);
+  std::cout << "Sum E^2 for PMHK: " << Akaiki_PMHK.first << '\n'
+            << "Akaiki for PMHK: " << Akaiki_PMHK.second << '\n';
 }
 
 void fill_matrixs(Matrix &X, Matrix &Y) {
@@ -37,7 +47,7 @@ void create_data(const int N) {
   const Matrix params({{0},
                        {0.22},
                        {-0.18},
-                       {-0.8},
+                       {0.08},
                        {1},
                        {0.5},
                        {0.5},
@@ -46,7 +56,7 @@ void create_data(const int N) {
   Matrix X(N, params.GetNumRows());
   X[0][0] = 1;
   for (int j = 1; j < X.GetNumColumns(); j++)
-    X[0][j] = (rand() % 50 - 25); //Первые полностью рандомные значения
+    X[0][j] = (rand() % 5 - 2.5); //Первые полностью рандомные значения
   Matrix Y(N, 1);
   Y[0][0] = (column_matrix(X[0]) * params)[0][0];
 
@@ -82,4 +92,14 @@ Matrix count_omega(Matrix &X, Matrix &Y, int params_count, int betta) {
                 (row_matrix(Y[i]) + (-1.0) * column_matrix(X[i]) * O);
   }
   return O;
+}
+
+std::pair<double, double> Akaiki(Matrix &X, Matrix &Y, Matrix &params) {
+  int N = X.GetNumRows();
+  double IKA = 0;
+  for (int i = 0; i < N; i++) {
+    IKA += pow((column_matrix(X[i]) * params + (-1.0) * row_matrix(Y[i]))[0][0],2);
+  }
+
+  return {IKA, (N * log(IKA) / log(2.7182818284) + 2 * N)};
 }
